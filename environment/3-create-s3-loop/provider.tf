c23f59ea2bdba39afb2131b9b982f8fd06a08546
izzuddinpulsifi <158985749+izzuddinpulsifi@users.noreply.github.com>
2024-05-20 11:31:52 +0800

    Delete environment/3-create-s3-loop directory

--- environment/3-create-s3-loop/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region  = var.region
-  profile = terraform.workspace
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-}
--- environment/3-create-s3-loop/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "region" {}
-
-variable "AWS_DEFAULT_PROFILE" {
-  type    = string
-  default = "default"
-}
